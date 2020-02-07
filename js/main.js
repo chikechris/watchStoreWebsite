70c4d8715022f5e79327f918e16f22d6cdb56aca
chikechris <cchike2017@gmail.com>
2020-02-07 06:46:53 -0500

    worked on skill section

--- js/main.js
+++ js/main.js
@@ -8,3 +8,16 @@ $('.slider-one')
     prevArrow: ".site-slider .slider-btn .prev",
     nextArrow: ".site-slider .slider-btn .next",
   })
+
+  /**second slider */
+$('.slider-two')
+  .not('slick-initialized')
+  .slick({
+    prevArrow: ".site-slider-two .prev",
+    nextArrow: ".site-slider-two .next",
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplaySpeed: 3000
+  })
+
+  window.addEventListener('load', () => document.querySelector('.preloader').classList.add('hidePreloader'))
